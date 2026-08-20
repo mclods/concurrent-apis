d5c41e15e71aede31c78e704c516c3c02604b37a
Sagnik Chakraborty <47282664+mclods@users.noreply.github.com>
2026-08-21 01:18:10 +0530

    Added producer consumer using semaphore tests. Fix error handing issue in Q.

--- src/main/java/org/mclods/producer_consumer_semaphore/PCSemDemo.java
+++ src/main/java/org/mclods/producer_consumer_semaphore/PCSemDemo.java
@@ -4,7 +4,21 @@ public class PCSemDemo {
     public static void run() {
         Q q = new Q();
 
-        new Thread(new Producer(q)).start();
-        new Thread(new Consumer(q)).start();
+        var producerThread = new Thread(new Producer(q), "Producer Thread");
+        var consumerThread = new Thread(new Consumer(q), "Consumer Thread");
+
+        producerThread.start();
+        consumerThread.start();
+
+        try {
+            producerThread.join();
+        } catch (InterruptedException e) {
+            System.out.println("InterruptedException Thrown: " + e);
+        }
+        try {
+            consumerThread.join();
+        } catch (InterruptedException e) {
+            System.out.println("InterruptedException Thrown: " + e);
+        }
     }
 }
--- src/main/java/org/mclods/producer_consumer_semaphore/Q.java
+++ src/main/java/org/mclods/producer_consumer_semaphore/Q.java
@@ -4,30 +4,44 @@ import java.util.concurrent.Semaphore;
 
 public class Q {
     private int n;
-    private static final Semaphore producerSem = new Semaphore(1);
-    private static final Semaphore consumerSem = new Semaphore(0);
+    private final Semaphore producerSem, consumerSem;
+    private boolean consumerSemAcquired, producerSemAcquired;
+
+    public Q() {
+        producerSem = new Semaphore(1);
+        consumerSem = new Semaphore(0);
+        producerSemAcquired = consumerSemAcquired = false;
+    }
 
     public int get() {
         try {
             consumerSem.acquire();
+            consumerSemAcquired = true;
         } catch (InterruptedException e) {
             System.out.println("InterruptedException caught" + e);
         }
 
-        System.out.println("Got " + n);
-        producerSem.release();
+        if (consumerSemAcquired) {
+            System.out.println("Got " + n);
+            producerSem.release();
+            producerSemAcquired = false;
+        }
         return n;
     }
 
     public void put(int n) {
         try {
             producerSem.acquire();
+            producerSemAcquired = true;
         } catch (InterruptedException e) {
             System.out.println("InterruptedException caught" + e);
         }
 
-        this.n = n;
-        System.out.println("Put " + n);
-        consumerSem.release();
+        if (producerSemAcquired) {
+            this.n = n;
+            System.out.println("Put " + n);
+            consumerSem.release();
+            consumerSemAcquired = false;
+        }
     }
 }
